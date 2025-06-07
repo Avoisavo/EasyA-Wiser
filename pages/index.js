109c974619b8ba16235a2c3ab6740686d8b82310
Cedric <cedricctf11a@gmail.com>
2025-06-08 02:23:17 +0800

    changed routing of landing page to kyc form page

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
             </p>
             <button
               className="bg-gradient-to-r from-blue-500 to-blue-700 text-white px-8 py-3 rounded-full text-lg font-semibold hover:from-blue-600 hover:to-blue-800 transition-all duration-300 shadow-lg shadow-blue-500/20 hover:shadow-blue-500/40 hover:scale-105"
-              onClick={() => router.push('/nocard')}
+              onClick={() => router.push('/kyc-form')}
             >
               Get Started
             </button>
